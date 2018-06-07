ab5220a99584483b62f2494697ad86b9a48912a6
daher7 <daher7@gmail.com>
2018-06-07 22:04:56 +0200

    cursor

--- js/02_eventos.js
+++ js/02_eventos.js
@@ -10,13 +10,27 @@ $(document).ready(function () { // Para comprobar que se carga jQuery.
         $(this).css("background", "blueviolet");
     }
 
-    function cambiarBordeBlanco(){
+    function cambiarBordeBlanco() {
         $(this).css("border", "2px dashed white").css("color", "white");
     }
 
-    function cambiarColorFondoRosa(){
+    function cambiarColorFondoRosa() {
         $(this).css("background", "pink");
     }
+
+    function cambiarBordeAmarillo() {
+        $(this).css("border", "4px solid yellow");
+        console.log("Borde Amarillo");
+    }
+
+    function moverBola() {
+        var sigueme = $("#sigueme");
+        console.log("X: " + event.clientX, "Y: " + event.clientY);
+        $('body').css("cursor", "none");
+        sigueme.css("left", event.clientX).css("top", event.clientY);
+    }
+
+
 
     var caja = $('#caja');
 
@@ -33,5 +47,29 @@ $(document).ready(function () { // Para comprobar que se carga jQuery.
     caja.click(cambiarBordeBlanco);
 
     caja.dblclick(cambiarColorFondoRosa);
+
+    // Focus y Blur
+    var nombre = $("#nombre");
+    var datos = $("#datos");
+
+    nombre.focus(function () {
+        $(this).css("border", "5px solid green");
+    });
+
+    nombre.blur(function () {
+        $(this).css("border", "5px dashed blue");
+        datos.text($(this).val()); // Para recoger el texto del input y meterlo en el div
+        datos.css("display", "block");
+    });
+
+    // Mousedown y Mouseup
+
+    datos.mousedown(cambiarBordeAmarillo); // Pulsas el boton del raton.
+
+    datos.mouseup(cambiarColorFondoRosa); // Sueltas el boton del raton.
+
+    // Mousemove. 
+
+    $(document).mousemove(moverBola);
 
 });
